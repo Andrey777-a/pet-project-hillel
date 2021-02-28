521f0b4fc685e805849b9e894f7649d66957e9db
Андрей Птицын <aandrey0795@gmail.com>
2021-02-28 16:11:58 +0200

    new

---
+++ src/main/java/com/example/springhillel/SpringHillelApplication.java
@@ -0,0 +1,16 @@
+package com.example.springhillel;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@Slf4j
+@SpringBootApplication
+public class SpringHillelApplication {
+
+	public static void main(String[] args) {
+		//SpringApplication.run(SpringHillelApplication.class, args);
+		log.info("Application has started");
+	}
+
+}
---
+++ src/test/java/com/example/springhillel/SpringHillelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springhillel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringHillelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
